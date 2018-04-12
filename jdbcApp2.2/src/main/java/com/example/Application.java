95cc98e695764dc8691ee24d90e83cc6d575a9bd
y1000mbg <y1000mbg@gmail.com>
2018-04-12 15:52:46 +0900

    初回コミット（JdbcTemplateを使った簡単なデータアクセス）

---
+++ jdbcApp2.2/src/main/java/com/example/Application.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplaate;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		
+		// JDBCのpreparedstatmentではプレースホルダで?を指定していたが
+		// Spring-bootでは:aなどで評議する
+		String sql = "SELECT :a + :b";
+
+		// プレースホルダーに値を反映
+		SqlParameterSource param = new MapSqlParameterSource()
+		.addValue("a",100)
+		.addValue("b", 200);
+		
+		// queryForObjectメソッドを使ってSQL実行結果をオブジェクトに変換
+		Integer result = jdbcTemplaate.queryForObject(sql, param, Integer.class);
+		
+		System.out.println("result = " + result);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
